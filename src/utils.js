c529bc465270be9b42dcba3ce8f4be94239c676b
elenapereztoril <elena.pereztoril@adyen.com>
2020-12-26 12:28:19 +0100

    Fix dropin request

--- src/utils.js
+++ src/utils.js
@@ -187,248 +187,19 @@ const getPaymentMethods = () =>
   //var custom = {
     //payment request here
   //}
-  var threeds1 = {
-   "merchantAccount" : "ElenaPerez",
-   "amount" : {
-      "value" : 36594,
-      "currency" : "EUR"
-   },
-   "reference" : "6002020688536",
-   "shopperEmail" : "victornaranjops4@gmail.com",
-   "shopperIP" : "89.47.236.60",
-   "shopperReference" : "pccomlogin3828475",
-   "browserInfo" : {
-      "screenWidth" : "2560",
-      "screenHeight" : "1080",
-      "colorDepth" : "24",
-      "userAgent" : "Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/87.0.4280.88 Safari\/537.36",
-      "timeZoneOffset" : "-60",
-      "language" : "es-ES",
-      "javaEnabled" : "false",
-      "acceptHeader" : "application\/json, text\/javascript, *\/*; q=0.01"
-   },
-   "shopperName" : {
-      "firstName" : "Francisco",
-      "lastName" : "Naranjo Gonzalez",
-      "gender" : "UNKNOWN"
-   },
-   "paymentMethod" : {
-      "encryptedCardNumber" : "test_5454545454545454",
-      "encryptedExpiryMonth" : "test_03",
-      "encryptedExpiryYear" : "test_2030",
-      "encryptedSecurityCode" : "test_737",
-      "holderName" : "Elena",
-      "type" : "scheme",
-      "storeDetails": false
-  },
-   "telephoneNumber" : "670530527",
-   "deliveryAddress" : {
-      "city" : "Huetor-Tajar",
-      "country" : "ES",
-      "houseNumberOrName" : ".",
-      "street" : "calle Alfredo nobel, 36 bajo",
-      "postalCode" : "18360",
-      "stateOrProvince" : "Granada"
-   },
-   "billingAddress" : {
-      "city" : "Huetor-Tajar",
-      "country" : "ES",
-      "houseNumberOrName" : ".",
-      "street" : "calle Alfredo nobel, 36 bajo",
-      "postalCode" : "18360",
-      "stateOrProvince" : "Granada"
-   },
-   "recurringProcessingModel" : "CardOnFile",
-   "shopperInteraction" : "Ecommerce",
-   "channel" : "Web",
-   "origin" : "http://localhost:3000/#/review",
-   "returnUrl" : "http://localhost:3000/#/review",
-}
-var atres=
-{
-   "additionalData" : {
-      "allow3DS2" : "true"
-   },
-   "amount" : {
-      "value" : 14800,
-      "currency" : "EUR"
-   },
-   "billingAddress" : {
-      "city" : "Glyfada",
-      "country" : "GR",
-      "houseNumberOrName" : "PSILOREITI 138",
-      "postalCode" : "16561",
-      "stateOrProvince" : "Attiki",
-      "street" : "PSILOREITI 138"
-   },
-   "captureDelayHours" : 0,
-   "channel" : "Web",
-   "deliveryAddress" : {
-      "city" : "Glyfada",
-      "country" : "GR",
-      "houseNumberOrName" : "PSILOREITI 138",
-      "postalCode" : "16561",
-      "stateOrProvince" : "Attiki",
-      "street" : "PSILOREITI 138"
-   },
-   "deliveryDate" : "2020-12-22T23:00:00.000Z",
-   "merchantAccount" : "ElenaPerez",
-   "paymentMethod" : {
-      "encryptedCardNumber" : "test_5454545454545454",
-      "encryptedExpiryMonth" : "test_03",
-      "encryptedExpiryYear" : "test_2030",
-      "encryptedSecurityCode" : "test_737",
-      "holderName" : "Elena",
-      "type" : "scheme",
-      "storeDetails": false
-  },
-   "reference" : "11744763",
-"returnUrl" : "http://localhost:3000/#/review",   "shopperEmail" : "v.dimitri@hotmail.com",
-   "shopperIP" : "5.54.179.18",
-   "shopperLocale" : "el_GR",
-   "shopperName" : {
-      "gender" : "UNKNOWN",
-      "lastName" : "VOGIATZIS",
-      "firstName" : "DIMITRIS"
-   },
-   "shopperReference" : "86839933",
-   "browserInfo" : {
-      "userAgent" : "Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/87.0.4280.88 Safari\/537.36",
-      "colorDepth" : 24,
-      "javaEnabled" : false,
-      "language" : "el-GR",
-      "screenHeight" : 864,
-      "screenWidth" : 1536,
-      "timeZoneOffset" : -120,
-      "acceptHeader" : "*\/*"
-   },
-   "applicationInfo" : {
-      "adyenLibrary" : {
-         "name" : "adyen-java-api-library",
-         "version" : "4.0.1"
-      }
-   },
-   "origin" : "http://localhost:3000/#/review"
-}
 
+   // "origin" : "http://localhost:3000/#/review",
+   // "returnUrl" : "http://localhost:3000/#/review",
+   // "paymentMethod" : {
+   //    "encryptedCardNumber" : "test_5454545454545454",
+   //    "encryptedExpiryMonth" : "test_03",
+   //    "encryptedExpiryYear" : "test_2030",
+   //    "encryptedSecurityCode" : "test_737",
+   //    "holderName" : "Elena",
+   //    "type" : "scheme",
+   //    "storeDetails": false
+   // },
 
-var TENDAM3d ={
-      "merchantAccount": "ElenaPerez",
-      "reference": "00044014",
-      "browserInfo": {
-          "screenWidth": 1920,
-          "screenHeight": 1080,
-          "colorDepth": 24,
-          "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
-          "timeZoneOffset": -60,
-          "language": "nl-NL",
-          "javaEnabled": false,
-          "acceptHeader": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9gzip, deflate, br"
-      },
-      "applicationInfo": {
-          "adyenPaymentSource": {
-              "name": "adyen-salesforce-commerce-cloud",
-              "version": "19.2.0"
-          },
-          "externalPlatform": {
-              "name": "SalesforceCommerceCloud",
-              "version": ""
-          }
-      },
-      "additionalData": {
-          "riskdata.deliveryMethod": "home-delivery-ES",
-          "riskdata.numberOfDaysShopperIsLoyal": "0201000200887005",
-          "riskdata.promotionCode": "N/A",
-          "riskdata.userStatus": "yes",
-          "riskdata.basket.item0": {
-              "category": "woman_blusas"
-          },
-          "allow3DS2": true
-      },
-      "channel": "web",
-      "origin" : "http://localhost:3000/#/review",
-      "returnUrl" : "http://localhost:3000/#/review",
-
-      "threeDS2RequestData": {
-          "notificationURL": ""
-      },
-      "amount": {
-          "currency": "EUR",
-          "value": 3999
-      },
-      "countryCode": "ES",
-      "deliveryAddress": {
-          "city": "Madrid",
-          "country": "ES",
-          "houseNumberOrName": "",
-          "postalCode": "28035",
-          "stateOrProvince": "Madrid",
-          "street": "Calle Grande 123"
-      },
-      "billingAddress": {
-          "city": "Madrid",
-          "country": "ES",
-         "houseNumberOrName": "",
-          "postalCode": "28035",
-          "stateOrProvince": "Madrid",
-          "street": "Calle Grande 123"
-      },
-      "paymentMethod" : {
-         "encryptedCardNumber" : "test_5454545454545454",
-         "encryptedExpiryMonth" : "test_03",
-         "encryptedExpiryYear" : "test_2030",
-         "encryptedSecurityCode" : "test_737",
-         "holderName" : "Elena",
-         "type" : "scheme",
-         "storeDetails": false
-     },
-      "telephoneNumber": "+34654123789",
-      "shopperEmail": "barry.boesveld@digitas.com",
-      "shopperName": {
-          "firstName": "Barry",
-          "gender": "UNKNOWN",
-          "infix": "",
-          "lastName": "Boesveld"
-      },
-      "shopperReference": "7200-0920556",
-      "shopperIP": "94.211.95.34",
-      "shopperLocale": "es_ES"
-  }
-
-var raku = {
-   "merchantAccount" : "ElenaPerez",
-   "reference" : "AD201222142915384bg8tSOq7UqxEg",
-   "shopperIP" : "91.126.131.67",
-   "shopperEmail" : "prodwiseau@wuaki.tv",
-   "shopperInteraction" : "Ecommerce",
-   "amount" : {
-      "value" : "199",
-      "currency" : "EUR"
-   },
-   "paymentMethod" : {
-      "encryptedCardNumber" : "test_5454545454545454",
-      "encryptedExpiryMonth" : "test_03",
-      "encryptedExpiryYear" : "test_2030",
-      "encryptedSecurityCode" : "test_737",
-      "holderName" : "Elena",
-      "type" : "scheme",
-      "storeDetails": false
-  },
-   "browserInfo" : {
-      "acceptHeader" : "*\/*",
-      "userAgent" : "Mozilla\/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/87.0.4280.88 Safari\/537.36",
-      "language" : "en-US",
-      "colorDepth" : 24,
-      "screenHeight" : 1050,
-      "screenWidth" : 1680,
-      "timeZoneOffset" : -60,
-      "javaEnabled" : false
-   },
-   "additionalData" : {
-
-   },
-   "recurringProcessingModel" : "CardOnFile"
-}
 // Posts a new payment into the local server
 const makePayment = (paymentMethod, config = {}) => {
     //const paymentsConfig = { ...config };
@@ -454,7 +225,7 @@ const makePayment = (paymentMethod, config = {}) => {
     console.log('paymentRequest: ',obj)
 
     //INFO: change paymentRequest to your new custom variable
-    return httpPost('payments', raku)
+    return httpPost('payments', paymentRequest)
         .then(response => {
             if (response.error) throw 'Payment initiation failed';
 
@@ -503,9 +274,9 @@ const paymentLinks = (paymentData) => {
 
     paymentRequest.amount.value = parseInt(getAmount());
     paymentRequest.amount.currency = getCurrencyCode();
-    paymentRequest.returnUrl = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.adyen.com%2Fblog%2Fkenya-and-the-original-mobile-payment-service&psig=AOvVaw1TfPJyNe6GnHZA-2NHJlCe&ust=1600937388458000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLiV-9Xy_usCFQAAAAAdAAAAABAD'
+    paymentRequest.returnUrl = defaultUrl();
     paymentRequest.origin = defaultUrl();
-    //paymentRequest.shopperReference = defaultShopperReference;
+    paymentRequest.shopperReference = defaultShopperReference;
 
     return httpPostnoJson('paymentLinks', paymentRequest)
         .then(response => {
@@ -514,7 +285,7 @@ const paymentLinks = (paymentData) => {
             return JSON.parse(response).url;
         })
         .catch(error => {
-            console.log('error on makePayment' + error)
+            console.log('error on paymentLinks' + error)
             throw Error(error);
         });
 };
@@ -530,7 +301,7 @@ const paymentLinksQR = (paymentData) => {
             return response;
         })
         .catch(error => {
-            console.log('error on makePayment' + error)
+            console.log('error on paymentLinksQR' + error)
             throw Error(error);
         });
 };
