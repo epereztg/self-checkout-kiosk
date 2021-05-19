deb399fff3903dc97965515e4410146b807b49d2
elenapereztoril <elena.pereztoril@adyen.com>
2021-05-19 14:51:27 +0200

    Add missing files

--- server/node/utils/config.js
+++ server/node/utils/config.js
@@ -2,9 +2,11 @@ const { CHECKOUT_APIKEY, MERCHANT_ACCOUNT } = process.env;
 
 const API_VERSION = 'v52';
 const CHECKOUT_URL = `https://checkout-test.adyen.com/${API_VERSION}`;
+const TERMINAL_URL = `https://terminal-api-test.adyen.com/sync`;
 
 module.exports = {
     CHECKOUT_APIKEY,
     CHECKOUT_URL,
+    TERMINAL_URL,
     MERCHANT_ACCOUNT
 };
