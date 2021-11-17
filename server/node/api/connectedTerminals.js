f4dabfe79e064e7811f10d541237c9fbb0105755
elenapereztoril <elena.pereztoril@adyen.com>
2021-11-17 17:05:56 +0100

    Bug fix

--- server/node/api/connectedTerminals.js
+++ server/node/api/connectedTerminals.js
@@ -3,7 +3,7 @@ const getPostParameters = require('../utils/getPostParameters');
 const handleCallback = require('../utils/handleCallback');
 
 module.exports = (res, request) => {
-    const params = getPostParameters('connectedTerminals', request);
+    const params = getPostParameters('/connectedTerminals', request);
 
     post(params, (error, response, body) => handleCallback({ error, response, body }, res));
 };
