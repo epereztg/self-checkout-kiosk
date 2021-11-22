e4afdc931f6b2b63f755f7044b772a174cbe8f59
elenapereztoril <elena.pereztoril@adyen.com>
2021-11-22 20:25:43 +0100

    Bug fix

--- server/node/api/clientKey.js
+++ server/node/api/clientKey.js
@@ -1,10 +1,10 @@
-//const { post } = require('request');
-//const getPostParametersPOS = require('../utils/getPostParametersPOS');
-//const handleCallback = require('../utils/handleCallback');
+const { post } = require('request');
+const getPostParametersPOS = require('../utils/getPostParametersPOS');
+const handleCallback = require('../utils/handleCallback');
 
 module.exports = (res, request) => {
-  //const params = getPostParameters('clientKey', request);
-  const res = process.env.CLIENT_KEY;
+  const params = getPostParameters('clientKey', request);
+  const clientkey = process.env.CLIENTKEY;
   ///post(params, (error, response, body) => handleCallback({ error, response, body }, res));
 
 };
