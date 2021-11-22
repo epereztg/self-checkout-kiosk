0d81316c3a2dd39590445493483780a843ccb4a1
elenapereztoril <elena.pereztoril@adyen.com>
2021-11-22 19:44:00 +0100

    Send to backend client key on node js

--- server/node/api/clientKey.js
+++ server/node/api/clientKey.js
@@ -3,7 +3,9 @@ const getPostParametersPOS = require('../utils/getPostParametersPOS');
 const handleCallback = require('../utils/handleCallback');
 
 module.exports = (res, request) => {
-    const params = getPostParametersPOS('/clientKey', request);
+  return process.env.CLIENT_KEY
+  //const port = process.env.PORT || 3000;
+    //const params = getPostParametersPOS('/clientKey', request);
 
-    post(params, (error, response, body) => handleCallback({ error, response, body }, res));
+    //post(params, (error, response, body) => //handleCallback({ error, response, body }, res));
 };
